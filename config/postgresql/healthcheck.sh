a19a03e7304bc4dff1dc14dca54fa36f4a62448e
Mark Gelya <gelyamark.2003@gmailcom>
2025-04-29 18:48:18 +0300

    Restore and update healthcheck.sh for postgres
    В 7cb0cdbd4a1e92acc81311ca63f635048486a892 был закомментирован exit 1
    --- это неправильно, исправил. Добавил user_db в healthcheck.

--- config/postgresql/healthcheck.sh
+++ config/postgresql/healthcheck.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-databases=("db" "product_db")
+databases=("db" "product_db" "user_db")
 
 for db in "${databases[@]}"; do
     if psql -h "localhost" -U "admin" -d "admin_db" -XAtc "SELECT 'ok' FROM pg_database WHERE datname='$db'" | grep 'ok'; then
         echo "Database '$db' is ready"
     else
         echo "Database '$db' is not ready"
-#        exit 1
+        exit 1
     fi
 done
 
