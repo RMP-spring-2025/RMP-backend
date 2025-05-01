44c8cdd150263c38fa184b423590e38b0c6d8bf7
Mark Gelya <gelyamark.2003@gmailcom>
2025-05-01 19:24:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,29 @@
-echo "RMP-user-service"
-cd RMP-user-service
-docker build -t rmp_us_build --file DockerfileBuild .
-docker run -v .:/home/gradle/project -v gradle_cache_rmp_us:/home/gradle/.gradle rmp_us_build
-cd ..
+#!/bin/bash
 
-echo "RMP-product-service"
+echo "RMP-product-service..."
 cd RMP-product-service
-docker build -t rmp_ps_build --file DockerfileBuild .
-docker run -v .:/home/gradle/project -v gradle_cache_rmp_ps:/home/gradle/.gradle rmp_ps_build
+bash build.sh
+PS_STATUS=$?
 cd ..
 
-echo "RMP-request-handler-service"
+echo "RMP-request-handler-service..."
 cd RMP-request-handler-service
-docker build -t rmp_rhs_build --file DockerfileBuild .
-docker run -v .:/home/gradle/project -v gradle_cache_rmp_rhs:/home/gradle/.gradle rmp_rhs_build
+bash build.sh
+RHS_STATUS=$?
 cd ..
+
+echo "RMP-user-service..."
+cd RMP-user-service
+bash build.sh
+US_STATUS=$?
+cd ..
+
+echo ""
+echo -n "RMP-product-service: "
+[ $PS_STATUS -eq 0 ] && echo "OK" || echo "Failed (code: ${PS_STATUS})"
+echo -n "RMP-request-handler-service: "
+[ $RHS_STATUS -eq 0 ] && echo "OK" || echo "Failed (code: ${RHS_STATUS})"
+echo -n "RMP-user-service: "
+[ $US_STATUS -eq 0 ] && echo "OK" || echo "Failed (code: ${US_STATUS})"
+[ $PS_STATUS -eq 0 ] && [ $RHS_STATUS -eq 0 ] && [ $US_STATUS -eq 0 ] && exit 0
+exit 1
